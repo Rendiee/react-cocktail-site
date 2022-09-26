c765be86879c2468a7142a702d7987c43c7a0313
Randy <90835943+Rendiee@users.noreply.github.com>
2022-09-26 15:20:28 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./component/Header/Header";
+
+function App() {
+  return (
+    <div>
+      <Header title="Randy" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './header.css';
+import logo from '../../assets/logo.png';
+import search from '../../assets/search.png';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <nav>
+                    <img className='header-logo' src={logo} alt='Logo : cocktail party' />
+                    <ul>
+                        <li><a href='#'>Accueil</a></li>
+                        <li><a href='#'>Histoire</a></li>
+                        <li><a href='#'>Cocktails</a></li>
+                        <li className='header-search'>
+                            <img className='header-search-icon' src={search} alt='search icon' />
+                            <input className='header-search-input' type="text" placeholder="Chercher ..." />
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
+}
+
+export default Header;
