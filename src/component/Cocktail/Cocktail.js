a25c0f66e07bd4cc0a1615c972304c8effb28a22
Rendie <90835943+Rendiee@users.noreply.github.com>
2022-10-05 00:55:15 +0200

    update cocktail list section + fetch api

--- src/component/Cocktail/Cocktail.js
+++ src/component/Cocktail/Cocktail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import { useState, useEffect } from "react";
 import CocktailCard from "../CocktailCard/CocktailCard";
 import './cocktail.css';
@@ -9,33 +10,47 @@ export default function Cocktail () {
     const [isLoaded, setIsLoaded] = useState(false);
     const [items, setItems] = useState([]);
 
+    const baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic';
+    const baseUrl2 = 'https://jsonplaceholder.typicode.com/users';
+
     const fetchData = async () => {
-        try {
-            const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic");
-            if(response.ok) {
-                console.log(response);
-                const data = await response.json();
-                console.log(data);
-                setItems(data);
-            }
-        }catch(error) {
-            setError(error);
-        }
+
+        // try {
+        //     const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic");
+        //     if(response.ok) {
+        //         console.log(response);
+        //         const data = await response.json();
+        //         console.log(data);
+        //         setItems(data);
+        //     }
+        // }catch(error) {
+        //     setError(error);
+        // }
+
+        axios.get(baseUrl).then(response => {
+            setItems(response.data.drinks);
+            console.log(response.data.drinks);
+        })
+
     }
 
     useEffect(() => {
-        fetchData()
+        fetchData();
     }, [])
 
     return(
-        <div className="cocktail-list">
-            {items.drinks.map(item => (
-                <CocktailCard
-                    uniqueId={item.idDrink}
-                    title={item.strDrink}
-                    img={item.strDrinkThumb+'/preview'}
-                />
-            ))}
+        <div>
+            <hr />
+            <h1 className="cocktail-title">Liste de cocktails</h1>
+            <div className="cocktail-list">
+                {items.map(item => (
+                    <CocktailCard
+                        key={item.idDrink}
+                        title={item.strDrink}
+                        img={item.strDrinkThumb+'/preview'}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
--- src/component/CocktailCard/CocktailCard.js
+++ src/component/CocktailCard/CocktailCard.js
@@ -1,12 +1,20 @@
 import React from "react"
 import './cocktailcard.css';
+import arrow from '../../assets/arrow.png';
 
 class CocktailCard extends React.Component {
     render() {
         return(
-            <div className="cocktail-card" key={this.props.uniqueId}>
-                <img src={this.props.img} alt='cocktail img' />
-                <p className="cocktail-card-title">{this.props.title}</p>
+            <div className="cocktail-card" key={this.props.key}>
+                <img className="cocktail-card-img" src={this.props.img} alt='cocktail img' />
+                <div className="cocktail-card-info">
+                    <p className="cocktail-card-title">{this.props.title}</p>
+                    <div className="cocktail-card-align-center">
+                        <small className="cocktail-card-small">Cocktail sans alcool</small>
+                        <button className="cocktail-card-button"></button>
+                    </div>
+                </div>
+                <div className="cocktail-card-square"></div>
             </div>
         )
     }
