ae14737c3bab84f019cc54844fd070bfcec1a377
Randy <90835943+Rendiee@users.noreply.github.com>
2022-10-05 13:11:05 +0200

    start navbar responsive

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './header.css';
 import logo from '../../assets/logo.png';
 import search from '../../assets/search.png';
-
+//https://levelup.gitconnected.com/how-to-create-a-responsive-navigation-bar-with-and-without-javascript-81f6d0671c6d
 class Header extends React.Component {
     render() {
         return (
             <header>
                 <nav className='navlist'>
                     <a href='#'><img className='header-logo' src={logo} alt='Logo : cocktail party' /></a>
-                    <ul>
+                    <ul className='nav-bar'>
                         <li><a href='#'>Accueil</a></li>
                         <li><a href='#'>Histoire</a></li>
                         <li><a href='#'>Cocktails</a></li>
@@ -17,6 +17,7 @@ class Header extends React.Component {
                             <img src={search} alt='search icon' />
                             <input className='header-search-input' type="text" placeholder="Chercher ..." />
                         </li>
+                        <li><a className='menu-icon'>&#9776;</a></li>
                     </ul>
                 </nav>
             </header>
