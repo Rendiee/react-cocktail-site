6d174526eaa7ebd632d97c0159a9a1ca3be4a36a
Rendie <90835943+Rendiee@users.noreply.github.com>
2022-10-07 00:00:38 +0200

    cocktail list reponsive

--- src/component/CocktailCard/CocktailCard.js
+++ src/component/CocktailCard/CocktailCard.js
@@ -9,10 +9,8 @@ class CocktailCard extends React.Component {
                 <img className="cocktail-card-img" src={this.props.img} alt='cocktail img' />
                 <div className="cocktail-card-info">
                     <p className="cocktail-card-title">{this.props.title}</p>
-                    <div className="cocktail-card-align-center">
-                        <small className="cocktail-card-small">Cocktail sans alcool</small>
-                        <button className="cocktail-card-button"></button>
-                    </div>
+                    <small className="cocktail-card-small">Cocktail sans alcool</small>
+                    <button className="cocktail-card-button"></button>
                 </div>
                 <div className="cocktail-card-square"></div>
             </div>
